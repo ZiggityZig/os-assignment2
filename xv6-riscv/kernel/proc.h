e7e9b790bea7772de51f710623f01c13d2200b61
ZiggityZig <zigi531@gmail.com>
2022-05-15 19:40:32 +0300

    init, task 2 completed

--- xv6-riscv/kernel/proc.h
+++ xv6-riscv/kernel/proc.h
@@ -24,6 +24,7 @@ struct cpu {
   struct context context;     // swtch() here to enter scheduler().
   int noff;                   // Depth of push_off() nesting.
   int intena;                 // Were interrupts enabled before push_off()?
+  int list_head_pid;          // the head of this spessific CPU RUNNABLE list, i added
 };
 
 extern struct cpu cpus[NCPU];
@@ -92,6 +93,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int next_pid;                // pointer to the next procces, i added
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
