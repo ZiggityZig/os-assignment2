a6360f621990fe7b0eeb6838c47b5fd2984a00b7
ZiggityZig <zigi531@gmail.com>
2022-05-23 12:19:33 +0300

    commented out function that was used for debugging

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -7,7 +7,7 @@
 #include "defs.h"
 #include <stdbool.h>
 
-uint64 MAX_UINT64 = 9446744073709551615;
+uint64 MAX_UINT64 = 94467440737095515;
 
 int UNUSED_list_head;
 int SLEEPING_list_head;
@@ -444,7 +444,7 @@ int fork(void)
   np->state = RUNNABLE;
   release(&np->lock);
   //-----i added-----
-  struct cpu *min_admittion_cpu = cpus[0];
+  struct cpu *min_admittion_cpu = &cpus[0];
   struct cpu *current_cpu;
   uint64 min_admittion = MAX_UINT64;
   for (current_cpu = cpus; current_cpu < &cpus[NCPU]; current_cpu++) {
@@ -759,7 +759,7 @@ void wakeup(void *chan)
         loop_var->state = RUNNABLE;
       }
       release(&loop_var->lock);
-      struct cpu *min_admittion_cpu = cpus[0];
+      struct cpu *min_admittion_cpu = &cpus[0];
       struct cpu *current_cpu;
       uint64 min_admittion = MAX_UINT64;
       for (current_cpu = cpus; current_cpu < &cpus[NCPU]; current_cpu++) {
